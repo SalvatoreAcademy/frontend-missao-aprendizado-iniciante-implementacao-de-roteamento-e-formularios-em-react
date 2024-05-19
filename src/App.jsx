e61c5a2306c0ac071d69ced9bd06dff0c55c93cc
Paulo Salvatore <salvatoregames@gmail.com>
2024-05-19 01:57:48 +0000

    `Header` adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import Header from './components/Header/Header'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />
+    element: (
+      <>
+        <div>
+          <Header />
+          <Outlet />
+        </div>
+      </>
+    ),
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
   }
 ])
 
