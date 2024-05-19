7589ba5e9e2288bfccf63272bdfd81e7278cb15c
Paulo Salvatore <salvatoregames@gmail.com>
2024-05-19 02:09:40 +0000

    Criação do `<form>`

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,3 +1,29 @@
 export default function Create() {
-  return <div>Create</div>
+  return (
+    <div>
+      <h1>Criar DevMon</h1>
+
+      <form>
+        <div>
+          <label htmlFor="nome">Nome*:</label><br />
+          <input type="text" id="nome" name="nome" placeholder="Insira o nome" />
+        </div>
+
+        <div>
+          <label htmlFor="imagem">Imagem (URL)*:</label><br />
+          <input type="text" id="imagem" name="imagem" placeholder="Insira a URL da imagem" />
+        </div>
+
+        <div>
+          <label htmlFor="evoluiPara">Evolui Para (opcional):</label><br />
+          <input type="text" id="evoluiPara" name="evoluiPara" placeholder="Insira o nome da evolução" />
+        </div>
+
+        <div>
+          <button type="submit">Criar</button>
+          <button type="reset">Reiniciar</button>
+        </div>
+      </form>
+    </div>
+  )
 }
